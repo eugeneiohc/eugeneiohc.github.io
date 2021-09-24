6afe22e7802f3af8456eb353b53d26c273a00ee9
Eugene Choi <eugeneiohc@hotmail.com>
2021-09-24 10:39:33 -0400

    eslint error

---
+++ src/components/ThemeProvider.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react';
+import { ThemeProvider as EmotionThemeProvider, useTheme as useEmotionTheme, Global, css } from '@emotion/react';
+import { theme } from '../theme';
+
+export const useTheme = () => useEmotionTheme() as typeof theme;
+
+export const ThemeProvider: FC = ({ children }) => (
+  <EmotionThemeProvider theme={theme}>
+    <Global
+      styles={css`
+        @import url('https://fonts.googleapis.com/css2?family=Roboto&family=Shippori+Mincho+B1:wght@400;700&display=swap');
+        body {
+          padding: 0;
+          margin: 0;
+        }
+        body * {
+          box-sizing: border-box;
+        }
+        h1,
+        h2,
+        h3,
+        h4,
+        h5 {
+          margin: 0;
+        }
+      `}
+    />
+    {children}
+  </EmotionThemeProvider>
+);
