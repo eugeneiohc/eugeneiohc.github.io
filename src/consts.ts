88023dca908c8bf71208b18f80672e723b3b3bbb
Eugene <eugeneiohc@hotmail.com>
2026-04-17 14:55:36 -0400

    updates

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Eugene Choi';
-export const SITE_DESCRIPTION = 'Notes on data engineering, architecture, and building things.';
+export const SITE_DESCRIPTION = 'Technical notes on data engineering and data infrastructure.';
